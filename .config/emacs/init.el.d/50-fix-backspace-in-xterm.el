c33692fedbea3c435f4154decc90c9394a2df097
Boni Lindsley <boni@lindsley.xyz>
2022-04-10 14:20:11 +0100

    fix(emacs): backspace not erase in XTerm

--- .config/emacs/init.el.d/50-fix-backspace-in-xterm.el
+++ .config/emacs/init.el.d/50-fix-backspace-in-xterm.el
@@ -1,13 +1,13 @@
-; Default of `normal-erase-is-backsapce` is `maybe`.
 ;
 ; In XTerm,
-; Backsapce sends `C-d` which maps to `delete-char` (forward).
 ; Delete sends `<deletechar>` which maps to `delete-forward-char`.
+; `C-d` sends `C-d` which maps to `delete-char` (forward).
+;
+; And by default,
+; Backsapce sends `C-d`
 ; `C-h` sends `DEL` which maps to `delete-backward-char`.
 
-; After setting the mode
-; `C-d` sends `C-d`
-; Delete sends `<deletechar>`
+; After setting the mode,
 ; Backspace sends `DEL`
 ; `C-h` sends C-h`.
 
@@ -18,11 +18,11 @@
   (cond
     (
       (string-equal term "xterm")
-      (normal-erase-is-backspace-mode 1)
+      (normal-erase-is-backspace-mode 0)
     )
     (
       (string-equal term "xterm-256color")
-      (normal-erase-is-backspace-mode 1)
+      (normal-erase-is-backspace-mode 0)
     )
   )
 )
