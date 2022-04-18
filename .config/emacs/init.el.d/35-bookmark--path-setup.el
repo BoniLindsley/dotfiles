47ce6ef98196adcc9bd2fcbdb47e8a39bcdf4258
Boni Lindsley <boni@lindsley.xyz>
2022-04-18 16:21:40 +0100

    fix(emacs): bookmark directory to XDG

---
+++ .config/emacs/init.el.d/35-bookmark--path-setup.el
@@ -0,0 +1,10 @@
+(eval-after-load 'org
+  (custom-set-variables
+    ;(SYMBOL EXP [NOW [REQUEST [COMMENT]]])
+
+    ;'(bookmark-default-file "~/.emacs.bmk")
+    '(bookmark-default-file
+       (expand-file-name ".emacs.bmk" user-emacs-directory)
+    )
+  )
+)
