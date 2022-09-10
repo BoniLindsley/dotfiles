beb73a3ef9885d8088c89b5640eac879ce4c45d3
Boni Lindsley <boni@lindsley.xyz>
2022-09-10 15:42:43 +0100

    update(emacs): bookmark filename

--- .config/emacs/init.el.d/35-bookmark--path-setup.el
+++ .config/emacs/init.el.d/35-bookmark--path-setup.el
@@ -3,8 +3,11 @@
     ;(SYMBOL EXP [NOW [REQUEST [COMMENT]]])
 
     ;'(bookmark-default-file "~/.emacs.bmk")
+    ;'(bookmark-default-file ; As early as Emacs v27.1.
+    ;   (expand-file-name "bookmarks" user-emacs-directory)
+    ;)
     '(bookmark-default-file
-       (expand-file-name ".emacs.bmk" user-emacs-directory)
+       (expand-file-name "bookmarks" user-emacs-directory)
     )
   )
 )
