22f41aef1c89e03bd19816607f01ce813f737540
Boni Lindsley <boni@lindsley.xyz>
2022-10-04 21:01:08 +0100

    fix(emacs): fractional duration in post-v26.1

--- .config/emacs/init.el.d/35-org--todo-list-appearance.el
+++ .config/emacs/init.el.d/35-org--todo-list-appearance.el
@@ -1,24 +1,44 @@
 (eval-after-load 'org
-  (custom-set-variables
-    ; Show schedule using "future" to mean "after current second".
-    ; Default is "after today".
-    ;'(org-agenda-todo-ignore-time-comparison-use-seconds nil)
-    '(org-agenda-todo-ignore-time-comparison-use-seconds t)
+  (progn
+    (custom-set-variables
+      ; Show schedule using "future" to mean "after current second".
+      ; Default is "after today".
+      ;'(org-agenda-todo-ignore-time-comparison-use-seconds nil)
+      '(org-agenda-todo-ignore-time-comparison-use-seconds t)
 
-    ; Hide item scheduled in the future.
-    ; Default shows all scheduled items in the todo list.
-    ;'(org-agenda-todo-ignore-scheduled nil)
-    '(org-agenda-todo-ignore-scheduled 'future)
+      ; Hide item scheduled in the future.
+      ; Default shows all scheduled items in the todo list.
+      ;'(org-agenda-todo-ignore-scheduled nil)
+      '(org-agenda-todo-ignore-scheduled 'future)
 
-    ; Use only hours (25h) instead of days (1d 1h).
-    '(org-time-clocksum-format '(
-      :hours "%d"
-      :require-hours t
-      :minutes ":%02d"
-      :require-minutes t
-    ))
-
+      ; Use only hours (25h) instead of days (1d 1h).
+      '(org-time-clocksum-format '(
+        :hours "%d"
+        :require-hours t
+        :minutes ":%02d"
+        :require-minutes t
+      ))
+    )
     ; Use fractions (1.5h) instead of minutes (1:30).
-    '(org-time-clocksum-use-fractional t)
+    (cond
+      (
+        (or
+          (> emacs-major-version 26)
+          (and
+            (= emacs-major-version 26)
+            (> emacs-minor-version 0)
+          )
+        )
+        (custom-set-variables
+          '(org-duration-format '(("h") (special . 2)))
+        )
+      )
+      ; Old variable for pre-26.1.
+      (t
+        (custom-set-variables
+          '(org-time-clocksum-use-fractional t)
+        )
+      )
+    )
   )
 )
