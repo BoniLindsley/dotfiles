2ec8ac97b3e6e3857157a8854d2845b54aa9bdc0
Boni Lindsley <boni@lindsley.xyz>
2023-08-03 22:41:27 +0100

    fix(python): use import in startup, not exec

--- .config/python/startup.py
+++ .config/python/startup.py
@@ -3,49 +3,29 @@
 # Standard libraries.
 import inspect
 import logging
+import os.path
 import platform
-import pathlib
 import sys
+
 
 _logger = logging.getLogger(__name__)
 
-
-# Required Python 3.6 for type hints.
+# Required Python 3.6 for explicit type hints on variables.
 # Required Python 3.6 `open` support for `pathlib.Path`.
 # Required Python 3.9 built-in templates such as `list[str]`.
-# Required Python 3.10 union type support written pipe.
-if tuple(map(int, platform.python_version_tuple())) < (3, 5, 0):
+# Required Python 3.10 union type support written with pipes.
+if tuple(map(int, platform.python_version_tuple())) < (3, 6, 0):
     _logger.warning(
-        "Not loading startup script. Python version 3.6 required."
+        "Not loading startup script. Python version 3.5 required."
     )
 else:
-    startup_path = pathlib.Path(
-        inspect.getsourcefile(lambda: None) or "."
-    )
-
-    site_path = startup_path.with_name("packages")
-    _logger.debug("Using packages in %s", site_path)
-    sys.path.insert(0, str(site_path))
-    del site_path
-
-    for package_path in sorted((startup_path.parent / "src").iterdir()):
-        for sys_path in (package_path / "src", package_path):
-            if sys_path.is_dir():
-                _logger.debug("Using packages in %s", sys_path)
-                sys.path.insert(0, str(sys_path))
-                break
-        del sys_path
-        del package_path
-
-    recurse_directory = startup_path.parent / (startup_path.name + ".d")
-    for script_path in sorted(recurse_directory.glob("*.py")):
-        # Requires Python 3.6 for for `open(pathlib.Path)`.
-        with open(str(script_path), encoding="utf-8") as script:
-            _logger.debug("Loading source at %s", script_path)
-            # Mimicking bash source.
-            exec(script.read())  # pylint: disable=exec-used
-        del script
-        del script_path
-    del recurse_directory
-
+    # Figure out where the python config directory is.
+    startup_path = inspect.getsourcefile(lambda: None) or "."
+    startup_dir = os.path.dirname(os.path.abspath(startup_path))
+    sys.path.insert(0, startup_dir)
+    # Do not expose local variables to REPL.
+    del startup_dir
     del startup_path
+
+    # Make exported variables available in REPL.
+    from startup_loader import *  # pylint: disable=unused-import
---
+++ .config/python/startup_loader.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Standard libraries.
+import importlib
+import inspect
+import logging
+import pathlib
+import sys
+
+
+__all__ = []
+_logger = logging.getLogger(__name__)
+
+startup_path = pathlib.Path(inspect.getsourcefile(lambda: None) or ".")
+
+site_path = startup_path.with_name("packages")
+_logger.debug("Using packages in %s", site_path)
+sys.path.insert(0, str(site_path))
+
+for package_path in sorted((startup_path.parent / "src").iterdir()):
+    for sys_path in (package_path / "src", package_path):
+        if sys_path.is_dir():
+            _logger.debug("Using packages in %s", sys_path)
+            sys.path.insert(0, str(sys_path))
+            break
+
+recurse_directory = startup_path.parent / "startup.py.d"
+sys.path.insert(0, str(recurse_directory))
+for script_path in sorted(recurse_directory.glob("*.py")):
+    imported_module = importlib.import_module(script_path.stem)
+    try:
+        module_variables = getattr(imported_module, "__all__")
+    except AttributeError:
+        module_variables = [
+            attribute
+            for attribute in dir(imported_module)
+            if attribute and attribute[0] != "_"
+        ]
+    globals().update(
+        {
+            attribute: getattr(imported_module, attribute)
+            for attribute in module_variables
+        }
+    )
+    __all__.extend(module_variables)
