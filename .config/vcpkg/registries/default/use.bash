e2bdaaf46c16268ae0ca40b56a1c3b7ca13f160a
Boni Lindsley <boni@lindsley.xyz>
2024-03-30 05:07:34 +0000

    fix(vcpkg): use script, wrong var

--- .config/vcpkg/registries/default/use.bash
+++ .config/vcpkg/registries/default/use.bash
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+__registry_baseline() {
+  cd "$1" || exit 1
+  git log -1 --format="%H"
+  cd "${OLDPWD}"
+}
+
 __main() {
   unset __main
 
@@ -8,11 +14,14 @@ __main() {
   local script_path="$(readlink -f "$0")"
   local script_dir="$(dirname $script_path)"
 
-  local registry_baseline
+  local registry_baseline="$(__registry_baseline "${script_dir}")"
+  local vcpkg_baseline="$(__registry_baseline "${VCPKG_ROOT}")"
 
-  registry_baseline="$(cd "${script_dir}" && git log -1 --format="%H")"
   export registry_baseline
-  cat "${script_dir}/vcpkg-configuration.json" | envsubst
+  export vcpkg_baseline
+  cat "${script_dir}/vcpkg-configuration.json" \
+    | envsubst \
+    > vcpkg-configuration.json
 }
 
 __main "$@"
