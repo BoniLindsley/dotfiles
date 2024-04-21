a02f521a5d3a41596d4d4ede9da3a2191fcb54b7
Boni Lindsley <boni@lindsley.xyz>
2024-04-21 21:14:06 +0100

    refactor(bonipy): type hints for floaty

--- .config/python/src/bonipy/src/bonipy/floaty/__main__.py
+++ .config/python/src/bonipy/src/bonipy/floaty/__main__.py
@@ -7,39 +7,85 @@ import logging
 import sys
 import tkinter
 import tkinter.font
-import typing as t
+import typing
+
+from typing import Dict, List, Union
 
 
 _logger = logging.getLogger(__name__)
 TRACE = 5
 
 
-def initialise_logging(*, verbosity: int) -> None:
-    logging.basicConfig()
+def set_logger_verbosity(
+    *, logger: logging.Logger, verbosity: int
+) -> None:
+    logging.addLevelName(1, "ALL")
     logging.addLevelName(TRACE, "TRACE")
+    verbosity_map = {
+        -2: logging.CRITICAL,
+        -1: logging.ERROR,
+        0: logging.WARNING,
+        1: logging.INFO,
+        2: logging.DEBUG,
+        3: TRACE,
+    }
+    minimum_verbosity = min(verbosity_map)
+    maximum_verbosity = max(verbosity_map)
+    verbosity = int(verbosity)
+    verbosity = min(maximum_verbosity, verbosity)
+    verbosity = max(minimum_verbosity, verbosity)
+    logging_level = verbosity_map.get(verbosity, logging.WARNING)
+    logger.setLevel(logging_level)
 
-    if verbosity > 0:
-        if verbosity == 1:
-            _logger.setLevel(logging.INFO)
-        elif verbosity == 2:
-            _logger.setLevel(logging.DEBUG)
-        else:
-            _logger.setLevel(TRACE)
+
+def set_up_logging(
+    *, logger: logging.Logger, verbosity: Union[None, int] = None
+) -> None:
+    formatter = logging.Formatter(
+        datefmt="%Y-%m-%d %H:%M:%S",
+        fmt="[{asctime}] [python/{name}] [{levelname[0]}] {message}",
+        style="{",
+    )
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    if verbosity is not None:
+        set_logger_verbosity(logger=logger, verbosity=verbosity)
 
 
-def get_font_height(size: int, *, size_to_height_cache: dict[int, int]) -> int:
+def parse_arguments(args: List[str]) -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        action="count",
+        default=0,
+        dest="verbosity",
+        help="Incrase verbosity.",
+    )
+    return parser.parse_args(args)
+
+
+def get_font_height(
+    size: int, *, size_to_height_cache: Dict[int, int]
+) -> int:
     height = size_to_height_cache.get(size)
     if height is not None:
         _logger.log(TRACE, "Font size %s has height %s.", size, height)
         return height
     height = tkinter.font.Font(size=size).metrics()["linespace"]
     size_to_height_cache[size] = height
-    _logger.log(TRACE, "Font size %s has height %s. (New.)", size, height)
+    _logger.log(
+        TRACE, "Font size %s has height %s. (New.)", size, height
+    )
     return height
 
 
 def get_maximum_font_size(
-    height: int, *, size_to_height_cache: None | dict[int, int] = None
+    height: int,
+    *,
+    size_to_height_cache: Union[None, Dict[int, int]] = None
 ) -> int:
     if size_to_height_cache is None:
         size_to_height_cache = {}
@@ -103,7 +149,7 @@ def get_maximum_font_size(
 
 
 class ClockLabel(tkinter.Label):
-    def __init__(self, *args: t.Any, **kwargs: t.Any) -> None:
+    def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
         super().__init__(*args, **kwargs)
         self.after_idle(self.update_clock)
 
@@ -114,23 +160,29 @@ class ClockLabel(tkinter.Label):
         self.config(text=new_text)
         _logger.log(TRACE, "Updating clock to %s.", new_text)
 
-        next_minute = now.replace(second=0) + datetime.timedelta(minutes=1)
+        next_minute = now.replace(second=0) + datetime.timedelta(
+            minutes=1
+        )
         new_update = next_minute - now
         _logger.log(
             TRACE,
             "Updating clock in %s seconds.",
             new_update // datetime.timedelta(seconds=1),
         )
-        self.after(new_update // datetime.timedelta(milliseconds=1), self.update_clock)
+        self.after(
+            new_update // datetime.timedelta(milliseconds=1),
+            self.update_clock,
+        )
 
 
 class WrappingFrame(tkinter.Frame):
-    def __init__(self, *args: t.Any, **kwargs: t.Any) -> None:
+    def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
         super().__init__(*args, **kwargs)
         clock_label = self.clock_label = ClockLabel(self)
         clock_label.pack(expand=True)
 
-        self.font_size_to_height_cache: dict[int, int] = {}
+        self.font_size_to_height_cache = {1: 2}
+        self.font_size_to_height_cache.clear()
 
         self.bind("<Configure>", self.on_configure)
 
@@ -144,18 +196,25 @@ class WrappingFrame(tkinter.Frame):
         old_font = label["font"]
         old_font_size = tkinter.font.Font(font=old_font)["size"]
         old_height = get_font_height(
-            old_font_size, size_to_height_cache=self.font_size_to_height_cache
+            old_font_size,
+            size_to_height_cache=self.font_size_to_height_cache,
         )
-        old_width = tkinter.font.Font(size=old_font_size).measure("00:00")
+        old_width = tkinter.font.Font(size=old_font_size).measure(
+            "00:00"
+        )
         _logger.log(
-            TRACE, "Resizing frame. Clock size was: %s x %s.", old_width, old_height
+            TRACE,
+            "Resizing frame. Clock size was: %s x %s.",
+            old_width,
+            old_height,
         )
 
         max_height = old_height * self.winfo_width() // old_width
         new_height = max(1, min(max_height, self.winfo_height()))
         if not new_height - 8 < old_height < new_height:
             new_font_size = get_maximum_font_size(
-                new_height, size_to_height_cache=self.font_size_to_height_cache
+                new_height,
+                size_to_height_cache=self.font_size_to_height_cache,
             )
             label.config(font=(None, new_font_size))
             _logger.log(
@@ -166,29 +225,23 @@ class WrappingFrame(tkinter.Frame):
             )
 
 
-class Application(tkinter.Tk):
-    def __init__(self, *args: t.Any, **kwargs: t.Any) -> None:
-        super().__init__(*args, **kwargs)
-        self.title(__package__)
-        self.wrapping_frame = WrappingFrame(self)
-        self.wrapping_frame.pack(expand=True, fill=tkinter.BOTH)
+def main(argv: Union[None, List[str]] = None) -> int:
+    if argv is None:
+        argv = sys.argv
 
+    arguments = parse_arguments(argv[1:])
+    set_up_logging(logger=_logger, verbosity=arguments.verbosity)
 
-def parse_arguments() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--verbose", "-v", action="count", default=0)
-    return parser.parse_args()
+    root = tkinter.Tk()
+    root.attributes("-topmost", "1")
+    root.geometry("640x240")
+    root.title(__package__ or "Floaty")
 
-
-def main() -> int:
-    arguments = parse_arguments()
-    initialise_logging(verbosity=arguments.verbose)
-
-    application = Application()
-    application.geometry("640x240")
+    wrapping_frame = WrappingFrame(root)
+    wrapping_frame.pack(expand=True, fill=tkinter.BOTH)
 
     _logger.log(TRACE, "Starting tkinter main loop.")
-    application.mainloop()
+    root.tk.mainloop()
 
     return 0
 
