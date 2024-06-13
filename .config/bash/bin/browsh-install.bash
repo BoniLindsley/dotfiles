d513b8f475bca08131547b70e18145fc80f9ccf0
Boni Lindsley <boni@lindsley.xyz>
2024-06-13 21:47:01 +0100

    add(browsh): install script

---
+++ .config/bash/bin/browsh-install.bash
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+__parse_arguments() {
+  while [[ 0 -lt $# ]]; do
+    case "$1" in
+    -q)
+      ((--__option_verbose))
+      shift
+      ;;
+    -v)
+      ((++__option_verbose))
+      shift
+      ;;
+    *)
+      log_error "Unexpected option -- $1"
+      return 1
+      ;;
+    esac
+  done
+}
+
+__main() {
+  local script_path
+  script_path="$(realpath "${BASH_SOURCE[0]}")" || return
+  local script_name
+  script_name="$(basename "${script_path}")" || return
+  local script_directory
+  script_directory="$(dirname "${script_path}")" || return
+
+  export LOGGING__LOG_NAME="${script_name}"
+  source "${script_directory}/../include/boni/logging.bash" || return
+
+  declare -i __option_verbose=0
+  __parse_arguments "$@" || return
+
+  logging__set_verbosity __option_verbose || return
+  log_trace "Running script at ${script_path}." || return
+
+  # Download source if not found.
+  : "${XDG_LOCAL_HOME:="${HOME}/.local"}"
+  local repository='https://github.com/browsh-org/browsh.git'
+  local version_tag='v1.8.2'
+  local source_name="${repository##*/}"
+  local source_name="${source_name%.git}"
+  local version_number="${version_tag#v}"
+  local full_name="${source_name}_${version_number}"
+
+  # Ensure install directory is clean and parent directory exists.
+  local install_directory="${XDG_LOCAL_HOME}/opt/${full_name}"
+  rm -dfr "${install_directory}" || return
+
+
+  local binary_path="${install_directory}/bin/browsh"
+  mkdir -p "${install_directory}/bin" || return
+  wget \
+    --output-document "${binary_path}" \
+    'https://github.com/browsh-org/browsh/releases/download/v1.8.2/browsh_1.8.2_linux_amd64' ||
+    return
+  chmod 755 "${binary_path}"
+}
+
+__main "$@"
