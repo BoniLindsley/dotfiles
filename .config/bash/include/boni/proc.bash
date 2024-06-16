7353ebccbce11946e3aa2f52b0085ac59eba0c4e
Boni Lindsley <boni@lindsley.xyz>
2024-06-16 15:29:34 +0100

    add(bash): proc mem read

--- .config/bash/include/boni/proc.bash
+++ .config/bash/include/boni/proc.bash
@@ -7,7 +7,7 @@ if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
   BONI__PROC__INCLUDED=
 fi
 
-proc__parse_maps() {
+proc__pid_maps() {
   local pid="${1?}"
 
   local data
@@ -17,9 +17,18 @@ proc__parse_maps() {
     start="0x${address%-*}"
     end="0x${address##*-}"
     length="$((end - start))"
-    data="$(xxd -len 1 -plain -s "$((start))" "/proc/${pid}/mem" 2>/dev/null)"
+    data="$(xxd -len 1 -plain -s "$((start))" "/proc/${pid}/mem" 2>/dev/null)" || return
     log_trace "Start: ${start} | End: ${end} | Length: ${length} | Data: '${data}' | Perms: ${perms} | Offset: ${offset} | Dev: ${dev} | Inode: ${inode} | Pathname: ${pathname}" || return
   done <"/proc/${pid}/maps"
+}
+
+proc__pid_mem() {
+  local pid="${1?}"
+  local start="${2?}"
+  local length="${3?}"
+
+  log_trace "Reading memory from PID: ${pid} | Start: ${start} | Length: ${length}" || return
+  dd bs=1 count="$((length))" if="/proc/${pid}/mem" skip="$((start))" || return
 }
 
 if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
@@ -96,7 +105,7 @@ main() {
   logging__set_verbosity "${arguments[verbose]}" || return
   log_trace "Running script: ${script_path}" || return
 
-  proc__parse_maps "${arguments[pid]}"
+  proc__parse_maps "${arguments[pid]}" || return
 }
 
 main "$@"
