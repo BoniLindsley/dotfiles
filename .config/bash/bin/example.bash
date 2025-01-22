80c346a32984e61b981a0f5aa4400deb644bb519
Boni Lindsley <boni@lindsley.xyz>
2025-01-22 22:49:39 +0000

    add(bash): example template script

--- .config/bash/bin/example.bash
+++ .config/bash/bin/example.bash
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
-  if [[ -n "${ARGPARSE__INCLUDED+defined}" ]]; then
-    return
-  fi
-  ARGPARSE__INCLUDED=
-fi
+# ========
+# Common source
+# ========
 
 argparse__remove_single_option() {
   : "${current_argument?}"
@@ -16,9 +13,78 @@ argparse__remove_single_option() {
   fi
 }
 
-if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
-  return
-fi
+logging__log() {
+  local level="$1"
+  shift
+
+  local current_level="${LOGGING__LOG_LEVEL:-30}"
+  if [[ "${level}" -lt "${current_level}" ]]; then
+    return
+  fi
+
+  local log_name="${LOGGING__LOG_NAME:-root}"
+  while [[ "$#" -gt 0 ]]; do
+    printf 1>&2 '%s: %s\n' "${log_name}" "$1" || return
+    shift
+  done
+}
+
+log_trace() {
+  logging__log 5 "$@"
+}
+
+log_debug() {
+  logging__log 10 "$@"
+}
+log_info() {
+  logging__log 20 "$@"
+}
+
+log_warning() {
+  logging__log 30 "$@"
+}
+log_error() {
+  logging__log 40 "$@"
+}
+
+log_critical() {
+  logging__log 50 "$@"
+}
+
+logging__set_verbosity() {
+  local verbosity="${1?}"
+  local level_name
+
+  export LOGGING__LOG_LEVEL
+  if [[ verbosity -ge 4 ]]; then
+    LOGGING__LOG_LEVEL=1
+    level_name='ALL'
+  elif [[ verbosity -ge 3 ]]; then
+    LOGGING__LOG_LEVEL=5
+    level_name='TRACE'
+  elif [[ verbosity -ge 2 ]]; then
+    LOGGING__LOG_LEVEL=10
+    level_name='DEBUG'
+  elif [[ verbosity -ge 1 ]]; then
+    LOGGING__LOG_LEVEL=20
+    level_name='INFO'
+  elif [[ verbosity -ge 0 ]]; then
+    LOGGING__LOG_LEVEL=30
+    level_name='WARNING'
+  elif [[ verbosity -ge -1 ]]; then
+    LOGGING__LOG_LEVEL=40
+    level_name='ERROR'
+  else
+    LOGGING__LOG_LEVEL=50
+    level_name='CRITICAL'
+  fi
+
+  log_debug "Setting log level to ${LOGGING__LOG_LEVEL} (${level_name})."
+}
+
+# ========
+# Actual implementation
+# ========
 
 __help() {
   declare REPLY
